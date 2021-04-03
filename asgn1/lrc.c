65e9ff7b5155359e71476a01d5d10c6b5ad0fe5e
Fabrice Kurmann <fkurmann@ucsc.edu>
2021-04-03 15:52:28 -0700

    Adding lrc draft 2

--- asgn1/lrc.c
+++ asgn1/lrc.c
@@ -9,26 +9,30 @@ int main(void) {
 	//typedef enum faciem { PASS, LEFT, RIGHT, CENTER } faces;
 	//faces die[] = {LEFT, RIGHT, CENTER, PASS, PASS, PASS};
 	
-	uint32_t seed, players;
+	uint32_t seed, num_players, center_value = 0;
 
 	//Scanner to enter random seed
 	printf("Random seed: ");
-	if (scanf("%u", &seed) <1) {//check constraints to this if statement
+	if (scanf("%u", &seed) < 1) {//check constraints to this if statement
 		fprintf(stderr, "Invalid random seed.\n");
 		return 1;
 	}
         
 	//Scanner to enter number of players
 	printf("Number of players: ");
-	if (scanf("%u", &players) < 1 || players > 14) {
-		fprintf(stderr, "Invalid number of players.n");
+	if (scanf("%u", &num_players) < 1 || num_players > 14) {
+		fprintf(stderr, "Invalid number of players\n");
 		return 1;
 	}
 	
 	srandom(seed); //Random number generator starting point
+	//int roll = random();
+	printf("%u\n", num_players);
+	
+	center_value++;
+	printf("%u\n", center_value);
 
-
-	printf("End of Program");
+	printf("End of Program\n");
 	
 
 	return 0;
