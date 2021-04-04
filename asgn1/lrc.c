88655074de6320071cdbc8e5ffec859fb28b3cce
Fabrice Kurmann <fkurmann@ucsc.edu>
2021-04-04 14:42:14 -0700

    Adding lrc.c draft 6

--- asgn1/lrc.c
+++ asgn1/lrc.c
@@ -5,31 +5,35 @@
 #include <stdlib.h>
 #include <assert.h>
 
+// Helper functions for left and right players
+static inline uint32_t left(uint32_t current_player, uint32_t num_players) {
+	return ((current_player + num_players -1) % num_players);
+}
+static inline uint32_t right(uint32_t current_player, uint32_t num_players) {
+	return ((current_player + 1) % num_players);
+}
 
 int main(void) {
 	//typedef enum faciem { PASS, LEFT, RIGHT, CENTER } faces;
 	//faces die[] = {LEFT, RIGHT, CENTER, PASS, PASS, PASS};
 	
-	uint32_t seed, num_rolls, center_value = 0;
-	uint8_t num_players, current_player = 0;
+	uint32_t seed, num_rolls, center_value = 0, num_players, current_player = 0, player_left, player_right, roll;
 
 	//Scanner to enter random seed
 	printf("Random seed: ");
-	if (scanf("%u", &seed) < 1) {//check constraints to this if statement
+	if (scanf("%u", &seed) < 1) {//check constraints to this if statement*********************************************************
 		fprintf(stderr, "Invalid random seed.\n");
 		return 1;
-	}
-        
+	} 
+	srandom(seed);
+
 	//Scanner to enter number of players
 	printf("Number of players: ");
 	if (scanf("%u", &num_players) < 1 || num_players > 14) {
 		fprintf(stderr, "Invalid number of players\n");
 		return 1;
 	}
-	
-	srandom(seed);
-	printf("%u\n", num_players);
-	
+		
 	
 	//****************************NO CHANGES ABOVE THIS LINE********************
 
@@ -39,7 +43,7 @@ int main(void) {
 		player_balance[i] = 3;
 	}
 
-	while (current_player < num_players) {      //Will eventually become while(num_players > 1)
+	while (num_players > 1) {      //Will eventually become while(num_players > 1)
 		num_rolls = 0;
 
 		if (player_balance[current_player] > 0) {
@@ -54,40 +58,53 @@ int main(void) {
 				num_rolls = 3;
 			}
 		}
-	
-		uint32_t roll;
 
 		//Roll the dice and conduct the resulting transfers depending on how many rolls a player gets
 		for (uint32_t i = 0; i < num_rolls; i++) {
-			roll = random() % 6
-			printf("%u\n", roll);
+			
+			//check that current player balance does not fall below zero during turn
+			if (player_balance[current_player] == 0) {
+				num_players--;
+				break;
+			}
 
-			if roll == 0 {
-				//give to left
+			roll = random() % 6;
+
+			if (roll == 0) {
+				player_balance[current_player]--;
+				player_left = left(current_player, num_players);
+				if (player_balance[player_left] == 0) { // Player resurrection means add to num_players
+					num_players++;
+				}
+				player_balance[player_left]++;
 			}
-			if roll == 1 {
-				//give to right
+			if (roll == 1) {
+				player_balance[current_player]--;
+				player_right = right(current_player, num_players);
+				if (player_balance[player_right] == 0) { // Player resurrection means add to num_players
+					num_players++;
+				}
+				player_balance[player_right]++;
 			}
-			if roll == 2 {
-				//give to center
-			center_value++;
+			if (roll == 2) {
+				player_balance[current_player]--;
+				center_value++;
 			}
+
 		}
-		current_player++;
-	}
-	printf("Number of last player: %u\n", current_player);
-	printf("Rolls for last player: %u\n", num_rolls);
-	printf("End of Program\n");
-	center_value++; //to shut up the pedantic error for now	
+		// check that current player balance didn't hit zero during turn
+		if (player_balance[current_player] == 0) {
+				num_players--;
+		}
 
+
+		current_player = player_right;
+		printf("Number of players remaining: %u\n", num_players);
+	}
+	//printf("Number of last player: %u\n", current_player);
+	//printf("Rolls for last player: %u\n", num_rolls);
+	printf("End of Program\n");
 	return 0;
 }
 
 
-
-static inline uint8_t left(uint8_t current_player, uint8_t, num_players) {
-	return ((current_player + num_players -1) % num_players);
-}
-static inline uint8_t right(uint8_t current_player, uint8_t, num_players) {
-	return ((current_player + 1) % num_players);
-}
