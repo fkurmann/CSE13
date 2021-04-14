562eaba69e0c8552f332957546297c8db716b7ab
Fabrice Kurmann <fkurmann@ucsc.edu>
2021-04-14 12:45:53 -0700

    Adding mathlib.c draft 2

--- asgn2/mathlib.c
+++ asgn2/mathlib.c
@@ -7,31 +7,12 @@
 #include <inttypes.h>
 
 #define EPSILON 0.001 //Lower precicion for now, MUST updata later maybe try #define EPSILON 1E-10
-// #define M_PI 3.14159265358979323846264338327950288 may not be needed
 
-int main(void) {
-    printf ("arcsin 0.5 = %lf \n", arcSin(0.5));
-    // printf ("arccos 0.5 = %lf \n", arcCos(0.5));
-    // printf ("arctan 0.5 = %lf \n", arcTan(0.5));
-    // printf ("log 0.5 = %lf \n", Log(0.5));
-
-    return 0;
-}
 // Helper Functions:
 
 // Abs is inspired from the provided funtion in Lecture 9
 double Abs(double x) {
     return x < 0 ? -x : x;
-}
-
-// Exp is inspired from the provided function on the CSE13S Piazza
-double Exp(double x) {
-    double term = 1, sum = 1;
-    for (int k = 1; Abs(term) > EPSILON; k++) {
-        term *= x/k;
-	sum += term;
-    }
-    return sum;
 }
 
 // Sqrt is inspired from the provided function on the CSE13S Piazza
@@ -49,6 +30,30 @@ double Sqrt(double x) {
 //     return Exp(y * Log(x));
 // }
 
+
+// Exp is inspired from the provided function on the CSE13S Piazza
+double Exp(double x) {
+    double term = 1, sum = 1;
+    for (int k = 1; Abs(term) > EPSILON; k++) {
+        term *= x/k;
+	sum += term;
+    }
+    return sum;
+}
+
+// Tester function main, temporary
+
+int main(void) {
+    printf ("arcsin 0.5 = %lf \n", arcSin(0.5));
+    printf ("arccos 0.5 = %lf \n", arcCos(0.5));
+    printf ("arctan 0.5 = %lf \n", arcTan(0.5));
+    printf ("log 2.0 = %lf \n", Log(2.0));
+    printf ("e^0.5 = %lf \n", Exp(0.5));
+
+    return 0;
+}
+
+
 // Library Functions:
 
 // arcSin function:
@@ -64,11 +69,11 @@ double arcSin(double x) {
 
 // arcCos function
 double arcCos(double x) {
-    double step_size = x, value = M_PI;
+    double step_size = x, value =((M_PI)/2) - x;
 
     for (int k = 1; step_size > EPSILON; k++) {
         step_size = step_size *(((2 * k - 1) * (2 * k - 1) * x * x)/((2 * k) * (2 * k + 1)));
-	value = value + step_size;
+	value = value - step_size;
     }
     return value;
 }
@@ -81,4 +86,12 @@ double arcTan(double x) {
     return value;
 }
 
-
+// Log function
+double Log(double x) {
+    double value = 1, step_size = x;
+    for (int k = 1; Abs(step_size) > EPSILON; k++) {
+        step_size = ((x - Exp(value))/Exp(value));
+	value = value + step_size;
+    }
+    return value;
+}
