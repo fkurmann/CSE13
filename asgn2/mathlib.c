058e655b58b31d3eb8676b318603af1b80d97232
Fabrice Kurmann <fkurmann@ucsc.edu>
2021-04-14 10:22:38 -0700

    Adding mathlib.c draft 1

--- asgn2/mathlib.c
+++ asgn2/mathlib.c
@@ -6,8 +6,25 @@
 #include <math.h>
 #include <inttypes.h>
 
+#define EPSILON 0.001 //Lower precicion for now, MUST updata later maybe try #define EPSILON 1E-10
+// #define M_PI 3.14159265358979323846264338327950288 may not be needed
 
-// Exp is inspired form the provided function on the CSE13S Piazza
+int main(void) {
+    printf ("arcsin 0.5 = %lf \n", arcSin(0.5));
+    // printf ("arccos 0.5 = %lf \n", arcCos(0.5));
+    // printf ("arctan 0.5 = %lf \n", arcTan(0.5));
+    // printf ("log 0.5 = %lf \n", Log(0.5));
+
+    return 0;
+}
+// Helper Functions:
+
+// Abs is inspired from the provided funtion in Lecture 9
+double Abs(double x) {
+    return x < 0 ? -x : x;
+}
+
+// Exp is inspired from the provided function on the CSE13S Piazza
 double Exp(double x) {
     double term = 1, sum = 1;
     for (int k = 1; Abs(term) > EPSILON; k++) {
@@ -26,3 +43,42 @@ double Sqrt(double x) {
     }
     return y;
 }
+
+// // Pow is inspired from the provided function in Lecture 9 MAY OR MAY NOT NEED
+// double Pow(double x, double y) {
+//     return Exp(y * Log(x));
+// }
+
+// Library Functions:
+
+// arcSin function:
+double arcSin(double x) {
+    double step_size = x, value = x;
+
+    for (int k = 1; step_size > EPSILON; k++) {
+        step_size = step_size *(((2 * k - 1) * (2 * k - 1) * x * x)/((2 * k) * (2 * k + 1)));
+	value = value + step_size;
+    }
+    return value;
+}
+
+// arcCos function
+double arcCos(double x) {
+    double step_size = x, value = M_PI;
+
+    for (int k = 1; step_size > EPSILON; k++) {
+        step_size = step_size *(((2 * k - 1) * (2 * k - 1) * x * x)/((2 * k) * (2 * k + 1)));
+	value = value + step_size;
+    }
+    return value;
+}
+
+// arcTan function
+double arcTan(double x) {
+    double value, plugin_value = (x/Sqrt((x * x + 1)));
+    value = arcSin(plugin_value);
+    
+    return value;
+}
+
+
