d2fb14248cce919239d63801a026cdd2c2778239
Fabrice Kurmann <fkurmann@ucsc.edu>
2021-04-15 19:05:13 -0700

    Adding asgn2 draft 6

--- asgn2/mathlib-test.c
+++ asgn2/mathlib-test.c
@@ -7,63 +7,72 @@
 #include <inttypes.h>
 #include <unistd.h>
 
-//#define OPTIONS "pi:"
+#define OPTIONS "asctl"
+#define FORMAT " %7.4lf % 16.8lf % 16.8lf % 16.10lf\n"
+#define LINES "  -            ------           -------       ----------\n"
 /*
 int main(int argc, char **argv) {
     int opt = 0;
-    while ((opt = getopt(argc, argc, OPTIONS)) != -1)  {
+    while ((opt = getopt(argc, argv, OPTIONS)) != -1)  {
         switch (opt) {
-        case'p' 
-	    printf("-p option. \n");
+	case'a': 
+    	    printf("  x            all              Library        Difference\n");
             break;
-        case'p':
-            printf("-p option. \n");
+        case's':
+    	    printf("  x            arcSin           Library        Difference\n");
             break;
+        case'c': 
+    	    printf("  x            arccos           Library        Difference\n");
+            break;
+        case't':
+    	    printf("  x            arctan           Library        Difference\n");
+            break;
+	case'l':
+    	    printf("  x            arclog           Library        Difference\n");
+	    break;
         }
     }
-    return 0
+    return 0;
 }
-*/
 
+*/
 
 int main(void) { 
 
     // Sin comparison:
     printf("  x            arcSin           Library        Difference\n");
-    printf("  -            ------           -------        ----------\n");
+    printf(LINES);
     for (double i = -1.0; i <= 1.0; i += 0.1) {
-	double my_fxn = arcSin(i), lib_fxn = asin(i);
-	
-        printf(" %7.4lf % 16.8lf % 16.8lf % 16.10lf\n", i, my_fxn, lib_fxn, (my_fxn - lib_fxn));
+	double my_fxn = arcSin(i), lib_fxn = asin(i);	
+        printf(FORMAT, i, my_fxn, lib_fxn, (my_fxn - lib_fxn));
     }
+
     // Cos comparison:
     printf("\n");
     printf("  x            arcCos           Library       Difference\n");
-    printf("  -            ------           -------       ----------\n");
+    printf(LINES);
     for (double i = -1.0; i <= 1.0; i += 0.1) {
-	double my_fxn = arcCos(i), lib_fxn = acos(i);
-	
-        printf(" %7.4lf % 16.8lf % 16.8lf % 16.10lf\n", i, my_fxn, lib_fxn, (my_fxn - lib_fxn));
+	double my_fxn = arcCos(i), lib_fxn = acos(i);	
+        printf(FORMAT, i, my_fxn, lib_fxn, (my_fxn - lib_fxn));
     }
 
     // Tan comparison:
     printf("\n");
     printf("  x            arcTan           Library       Difference\n");
-    printf("  -            ------           -------       ----------\n");
+    printf(LINES);
     for (double i = 1.0; i <= 10.0; i += 0.1) {
 	double my_fxn = arcTan(i), lib_fxn = atan(i);
-	
-        printf(" %7.4lf % 16.8lf % 16.8lf % 16.10lf\n", i, my_fxn, lib_fxn, (my_fxn - lib_fxn));
+        printf(FORMAT, i, my_fxn, lib_fxn, (my_fxn - lib_fxn));
     }
-  
+
     // Log comparison:
     printf("\n");
     printf("  x            Log              Library       Difference\n");
-    printf("  -            ------           -------       ----------\n");
+    printf(LINES);
     for (double i = 1.0; i <= 10.0; i += 0.1) {
 	double my_fxn = Log(i), lib_fxn = log(i);
 	
-        printf(" %7.4lf % 16.8lf % 16.8lf % 16.10lf\n", i, my_fxn, lib_fxn, (my_fxn - lib_fxn));
+        printf(FORMAT, i, my_fxn, lib_fxn, (my_fxn - lib_fxn));
     }
     
     return 0;
--- asgn2/mathlib.c
+++ asgn2/mathlib.c
@@ -6,7 +6,7 @@
 #include <math.h>
 #include <inttypes.h>
 
-#define EPSILON 0.0000000000001 //Lower precicion for now, MUST updata later maybe try #define EPSILON 1E-10
+#define EPSILON 1E-10
 
 // Helper Functions:
 
@@ -17,41 +17,41 @@ double Abs(double x) {
 
 // Sqrt is inspired from the provided function on the CSE13S Piazza
 double Sqrt(double x) {
-    double y = 1.0; 
-    assert(x >= 0);
-    for (double guess = 0.0; Abs(y - guess) > EPSILON; y = (y + x / y) / 2.0) {
-        guess = y;
+    double value = 1.0; 
+    for (double guess = 0.0; Abs(value - guess) > EPSILON; value = (value + x / value) / 2.0) {
+        guess = value;
     }
-    return y;
+    return value;
 }
 
 // Exp is inspired from the provided function on the CSE13S Piazza
 double Exp(double x) {
-    double term = 1, sum = 1;
-    for (int k = 1; Abs(term) > EPSILON; k++) {
-        term *= x/k;
-	sum += term;
+    double step_size = 1, value = 1;
+    for (int k = 1; Abs(step_size) > EPSILON; k++) {
+        step_size *= x/k;
+	value += step_size;
     }
-    return sum;
+    return value;
 }
 
 
-// Library Functions:
+// Homemade Library Functions:
 
-// arcSin function:
+// arcSin function, using Taylor Series to solve for inverse sin
 double arcSin(double x) {
-    if (Abs(x) > 0.8) { // To prevent larger error when x approaches 1, use trig property provided in Piazza
+    // To prevent larger error when x approaches 1, use trig property provided in Piazza
+    if (Abs(x) > 0.8) { 
         double value = ((M_PI)/2), plugin_value = (Sqrt((1 - x * x)));
         
-	value -= arcSin(plugin_value); // Recursion to the rescue!
+	value -= arcSin(plugin_value); // Recusivly call arcSin of a value further from |1|
 
 	if (Abs(x) != x) { // Since sign of x is removed in plugin_value translation, it is added back into consideration here
 	    value *= (-1);
-        }
-        
+        } 
 	return value;
     }
 
+    // Normal case of arcSin, when input to function is not close to |1|.
     else {
         double step_size = x, value = x;
         for (int k = 1; Abs(step_size) > EPSILON; k++) {
@@ -62,19 +62,18 @@ double arcSin(double x) {
     }
 }
 
-// arcCos function
+// arcCos function, using the trig property (arcCos = Pi/2 - arcSin), thus calling arcSin to solve 
 double arcCos(double x) {
-    double value = ((M_PI)/2);
-    return (value - arcSin(x));
+    return (((M_PI)/2) - arcSin(x));
 }
 
-// arcTan function
+// arcTan function, using the trip property (arcTan = arcSin ((x)/sqrt(x^2 + 1)), thus calling arcSin to solve
 double arcTan(double x) {
     double plugin_value = (x/Sqrt((x * x + 1)));
     return (arcSin(plugin_value));
 }
 
-// Log function
+// Log function, using Newton's method to iterativly solve for ln(x)
 double Log(double x) {
     double value = 1, step_size = x;
     for (int k = 1; Abs(step_size) > EPSILON; k++) {
